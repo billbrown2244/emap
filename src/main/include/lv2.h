c46ceb7d662907fca9fdf5e21fd2516167f6adaa
Bill Brown <billbrown@colorfulsoftware.com>
2014-07-16 09:35:27 -0500

    working on lv2

---
+++ src/main/include/lv2.h
@@ -0,0 +1,10 @@
+#ifndef SIN_SYNTH
+#define SIN_SYNTH
+
+#include "lv2-c++-tools/lv2.h"
+#include <lv2-c++-tools/lv2gui.hpp>
+
+#define EMAP_URI "http://www.colorfulsoftware.com/emap/lv2"
+#define EMAP_UI_URI "http://www.colorfulsoftware.com/emap/lv2ui"
+
+#endif
